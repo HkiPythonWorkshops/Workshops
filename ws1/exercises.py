a0d3b0861aa76aed50b647c5fcdfafb3d68ba939
shusso <shusso@iki.fi>
2015-02-15 19:58:31 +0200

    couple of exercices

--- ws1/exercises.py
+++ ws1/exercises.py
@@ -3,4 +3,31 @@
 print "hello world"
 
 
+###############################################
+##########         EXERCISES         ##########
+###############################################
+'''
+Exercise 1
+            -print your name to the screen
+            -print "i'm having fun learning Python!" to the screen
+            -print ten asterix (*-sign) to the screen
+'''
 
+print "Samuel"
+print "I'm having fun learning Python!"
+print "**********" #or
+print "*" * 10
+
+
+'''
+Exercise 2
+            -print "we  have  really  simple  formatting!" to the screen" (hit space-key couple of times between word to get basic formatting)
+            -
+            -
+            -print pyramid made out of asterix (*) signs to the screen
+'''
+print "we  have  really  simple  formatting!"
+
+print "   *"
+print "  ***"
+print " *****"
