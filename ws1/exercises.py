b5b082550a22cad71c0419ef6b8113f0d266221c
shusso <shusso@iki.fi>
2015-02-15 20:50:24 +0200

    more exercices

--- ws1/exercises.py
+++ ws1/exercises.py
@@ -9,25 +9,58 @@ print "hello world"
 '''
 Exercise 1
             -print your name to the screen
-            -print "i'm having fun learning Python!" to the screen
+            -print "is having fun learning Python!" to the screen
             -print ten asterix (*-sign) to the screen
 '''
-
-print "Samuel"
-print "I'm having fun learning Python!"
-print "**********" #or
-print "*" * 10
+print "__exercise 1__"
 
 
 '''
 Exercise 2
-            -print "we  have  really  simple  formatting!" to the screen" (hit space-key couple of times between word to get basic formatting)
-            -
-            -
-            -print pyramid made out of asterix (*) signs to the screen
-'''
-print "we  have  really  simple  formatting!"
+            -print "we  can  have  really  simple  formatting!" to 
+             the screen" (hit the space-key couple or more 
+             times between words)
+            -print 'and we can use the apostrophe sign for printting too'
+            -print empty string to the screen using "" or ''
+            -print "or use triple apostrophe signs" using three ' signs
+             in the beginning and in the end of the sentence
 
-print "   *"
-print "  ***"
-print " *****"
+'''
+print "__exercise 2__"
+
+
+'''
+Exercise 3
+            -print the sentences "now this is.." <hit the enter key> "..something weird" to
+             the screen using the apostrophe sign and see that you can 
+             use the triple apostrophe sign for formatting too
+            -insert couple of spaces and hit the enter key couple of times to the 
+             previous sentence
+            -create a simple pyramid made out of asterix (*) signs to the screen 
+             with print and using the triple apostrophe sign and simple formatting
+'''
+print "__exercise 3__"
+
+
+'''
+Exercise 4
+            -calculate the value of 4+4
+            -..and notice that after running the file we don't see the answer to
+             that calculation on the screen! Add the print statement beginning
+             of the line. But don't add the " or ' signs!
+            -print the value of 42/8.0 to the screen by combining print and
+             the actual calculation
+            -what is the value of the following calculation: 3*8+(100-75)-7
+'''
+print "__exercise 4__"
+
+
+'''
+Exercise 5
+            -
+            -
+            -
+            -
+            -
+'''
+print "__exercise 5__"
