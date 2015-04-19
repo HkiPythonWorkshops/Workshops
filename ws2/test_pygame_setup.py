a02615ac66ef074c035d36ddff077a47d1041978
Samuel Husso <shusso@iki.fi>
2015-04-19 21:07:34 +0300

    material, test pygame setup

--- ws2/test_pygame_setup.py
+++ ws2/test_pygame_setup.py
@@ -1,30 +1,44 @@
 import pygame
+from pygame.locals import *
 import sys
 
-pygame.init()
-screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption('pyGame test Window')
+def quit_pygame():
+    print("Exiting pyGame..")
+    pygame.quit()
+    sys.exit()
 
-background = pygame.Surface(screen.get_size())
-background = background.convert()
-background.fill((100, 100, 142))
 
-font = pygame.font.Font(None, 42)
-text = font.render("Hello, everything seems to be OK", 1, (250, 250, 250))
-textpos = text.get_rect()
-textpos.centerx = background.get_rect().centerx
-textpos.centery = background.get_rect().centery
+def main():
+    # Initialise screen
+    pygame.init()
+    screen = pygame.display.set_mode((800, 600))
+    pygame.display.set_caption('pyGame test Window')
 
-background.blit(text, textpos)
+    # Fill background
+    background = pygame.Surface(screen.get_size())
+    background = background.convert()
+    background.fill((100, 100, 142))
 
-screen.blit(background, (0, 0))
-pygame.display.flip()
+    # Display some text
+    font = pygame.font.Font(None, 42)
+    text = font.render("Hello, everything seems to be OK", 1, (250, 250, 250))
+    textpos = text.get_rect()
+    textpos.centerx = background.get_rect().centerx
+    background.blit(text, textpos)
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            print("Exiting pyGame..")
-            pygame.quit()
-            sys.exit()
+    # Blit everything to the screen
     screen.blit(background, (0, 0))
     pygame.display.flip()
+
+    # Event loop
+    while 1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quit_pygame()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    quit_pygame()
+        screen.blit(background, (0, 0))
+        pygame.display.flip()
+
+if __name__ == '__main__': main()
